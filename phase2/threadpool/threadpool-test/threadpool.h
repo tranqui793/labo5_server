8f9f2b4c4cef45d346583ffc592c9488a55d75d1
Adam <adam.zouari@heig-vd.ch>
2018-06-01 12:24:20 +0200

    start implementation of threadpool

--- phase2/threadpool/threadpool-test/threadpool.h
+++ phase2/threadpool/threadpool-test/threadpool.h
@@ -9,10 +9,20 @@ class ThreadPool;
 
 class Worker : public QThread {
 
+public:
+
     Worker(int id):id(id){}
     // fait appel au run de ThreadPoolRunner
 
-protected:
+    void run(){
+
+        while(true){
+
+
+        }
+    }
+
+    protected:
     int id;
 };
 
@@ -27,8 +37,12 @@ public:
     void start(Runnable* runnable){
 
         if(workers.size() < workers.capacity()){
+            int id = 0;
+            Worker worker = new Worker(id++);
+            workers.push_back(worker);
+            workers.at(id)->start();
+        }else{
 
-            workers.push_back(new Worker());
         }
 
         // cree un thread ou reutilise un existant si un thread
