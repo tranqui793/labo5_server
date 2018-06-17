ef52cd707cbbbf3fcf8fb9e637ed0e386f6a49d4
olliens <oussama.lagha@heig-vd.ch>
2018-06-17 12:14:44 +0200

    modifcation de readerwritercache

--- phase3/cache/fileserver/readerwritercache.cpp
+++ phase3/cache/fileserver/readerwritercache.cpp
@@ -3,21 +3,58 @@
 #include <QDateTime>
 #include <QList>
 #include <QDebug>
-
+/**
+*   \brief          Constructeur de ReaderWriterCache
+*
+*/
 ReaderWriterCache::ReaderWriterCache(int invalidationDelaySec, int staleDelaySec):
     invalidationDelaySec(invalidationDelaySec), staleDelaySec(staleDelaySec)
 {
+    timer=new InvalidationTimer(this);
+    timer->start();
 }
-
+/**
+*   \brief          Destructeur
+*/
 ReaderWriterCache::~ReaderWriterCache()
 {
-
+    timer->requestInterruption();
+    delete timer;
 }
-
+/**
+*   \brief
+*   \param response permet l'ajout au map
+*/
 void ReaderWriterCache::putResponse(Response &response) {
-}
+    lock.lockWriting();
+        //pour chaque nouvelle requete on cree une struct avec le time courrant
 
+    TimestampedResponse response_struct(response,timer->time);
+
+    //l'ajout dans le cache
+    //map prend string et TimestampedResponse donc on peut pas stockee la request instance on met le path
+    map.insert(response.getRequest().getFilePath(),response_struct);
+
+    lock.unlockWriting();
+}
+/**
+*   \brief          permet de recuperer la resonse si elle exist
+*   \param request  la requete pour la quelle on veut recupere la response
+*/
 Option<Response> ReaderWriterCache::tryGetCachedResponse(Request &request) {
+    lock.lockReading();
     Option<Response> res = Option<Response>::none();
+
+    //on essaye de recuperer l'element
+
+   QHash<QString,TimestampedResponse>::const_iterator i= map.find(request.getFilePath());
+    if(i !=map.end()){
+        //si on a une response on met a jour le le timerstamp et on recupere la response
+        res=Option<Response>::some(i.value());
+        i.timestamp=timer->time;
+    }
+
+
+    lock.unlockReading();
     return res;
 }
--- phase3/cache/fileserver/readerwritercache.h
+++ phase3/cache/fileserver/readerwritercache.h
@@ -9,34 +9,61 @@
 #include "readerwriterlock.h"
 
 
+/** \class      rederwritercache
+*   \authors    Adam Zouari et Oussama Lagha
+*   \date       15 juin 2018
+*   \brief      modification du readerwritercache afin de pouvoir ajouter et recupere des responses
+*/
 class ReaderWriterCache
 {
 private:
 	struct TimestampedResponse {
 		Response response;
-		long timestamp;
+        long timestamp;
+     /**
+    *   \brief              Constructeur de struct TimestampedResponse
+    *   \param res          response enregistre la map
+    *   \param timestamp    l'heure de la derniere utilisation
+    */
+        TimestampedResponse(Response res,long timestamp):response(res),timestamp(timestamp){}
 	};
 
 	class InvalidationTimer: public QThread {
 		friend ReaderWriterCache;
 
 	private:
-		ReaderWriterCache* cache;
+        ReaderWriterCache* cache;
 
 	public:
-		InvalidationTimer(ReaderWriterCache* cache): cache(cache) {}
+        InvalidationTimer(ReaderWriterCache* cache): cache(cache),time(0) {}
+        unsigned time;          //represente le time courrant
 
 	protected:
 		void run() {
-			// TODO
+
+            while(true){
+                QHash<QString, TimestampedResponse>::iterator iterator;       //iterator pour map
+                for(iterator = cache->map.begin(); iterator != cache->map.end(); ){
+                    struct TimestampedResponse value = iterator.value();
+
+                    if((time - value.timestamp) < cache->staleDelaySec){            //si on a pas depasser le delai on verifie le suivant
+                        iterator++;
+                    }else{
+                       iterator = cache->map.erase(iterator);                       //sinon on efface
+                    }
+                }
+
+                QThread::sleep(cache->invalidationDelaySec);
+                time += cache->invalidationDelaySec;
+            }
 		}
 	};
 
-	QHash<QString, TimestampedResponse> map;
-	int invalidationDelaySec;
-	int staleDelaySec;
-	InvalidationTimer* timer;
-	ReaderWriterLock lock;
+    QHash<QString, TimestampedResponse> map;    //sert a enregistrer les response
+    int invalidationDelaySec;       //interval entre deux verification
+    int staleDelaySec;              //temps de validité
+    InvalidationTimer* timer;       //timer pour la validation
+    ReaderWriterLock lock;          //gére tout les priorité de reader et writer
 
 public:
 	ReaderWriterCache(int invalidationDelaySec, int staleDelaySec);
